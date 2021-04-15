6cce39edbf6b1786ac88530ca92383b3e935f665
crennicke <crennicke@wisc.edu>
2021-04-15 12:03:07 -0500

    4/15
    I added some keys and lights.  New skybox and such.  Still working out kinks and things.  Trying to get it to where if you touch the key the light lights up.

---
+++ Dungeoneering/Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    bool redKey = false;
+    bool blueKey = false;
+    bool purpleKey = false;
+    bool greenKey = false;
+
+    public void SetRedKey()
+    {
+        redKey = true;
+    }
+    public void SetBlueKey()
+    {
+        blueKey = true;
+    }
+    public void SetGreenKey()
+    {
+        greenKey = true;
+    }
+    public void SetPurpleKey()
+    {
+        purpleKey = true;
+    }
+
+    public bool AllKeysCollected()
+    {
+        if(redKey && greenKey && purpleKey && blueKey)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Dungeoneering/Assets/Scripts/KeyInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInteraction : MonoBehaviour
+{
+    public GameObject globalVars;
+    public string color = "red";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        globalVars = GameObject.Find("GlobalVars");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate((transform.right + Vector3.right) * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (color.Equals("blue"))
+        {
+            globalVars.GetComponent<GlobalVars>().SetBlueKey();
+        }
+        else if (color.Equals("red"))
+        {
+            Debug.Log("RED IS TOUCHED");
+            globalVars.GetComponent<GlobalVars>().SetRedKey();
+        }
+        else if (color.Equals("purple"))
+        {
+            globalVars.GetComponent<GlobalVars>().SetPurpleKey();
+        }
+        else if (color.Equals("green"))
+        {
+            globalVars.GetComponent<GlobalVars>().SetGreenKey();
+        }
+        Destroy(gameObject);
+    }
+}
