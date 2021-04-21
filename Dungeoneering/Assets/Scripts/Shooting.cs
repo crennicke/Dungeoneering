9ecf69730c4d610591a8b9c93a0be261d6731273
Joshkutch <kutchenriter@wisc.edu>
2021-04-21 16:16:17 -0500

    Simon Says button clean up

---
+++ Dungeoneering/Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics_Shooting : MonoBehaviour
+{
+	public Rigidbody projectile;
+    public Transform shotPos;
+    public float shotForce = 1000f;
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// movement
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(new Vector3(h, 0f, v));
+
+        // shooting
+        if(Input.GetButtonUp("Fire1"))
+        {
+            Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+            shot.AddForce(shotPos.forward * shotForce);
+        }
+    }
+}
